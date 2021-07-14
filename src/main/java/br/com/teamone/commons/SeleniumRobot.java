a15be6f6a891961c15b65913c5e891971e8ea1fc
Marcus Silva <Marcus.Silva@yaman.com.br>
2021-07-14 14:09:35 -0300

    Adicionado robot

---
+++ src/main/java/br/com/teamone/commons/SeleniumRobot.java
@@ -0,0 +1,14 @@
+package br.com.teamone.commons;
+
+import br.com.teamone.settings.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SeleniumRobot extends BaseTest {
+
+    public void esperaFicarVisivelEClica(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+}
