7bfc477c99de87f1d03f37657021dd94876be173
Marcus Silva <Marcus.Silva@yaman.com.br>
2021-07-14 14:31:48 -0300

    Adicionado robot certo e uma linha

--- src/main/java/br/com/teamone/commons/SeleniumRobot.java
+++ src/main/java/br/com/teamone/commons/SeleniumRobot.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+
 public class SeleniumRobot extends BaseTest {
 
     public void esperaFicarVisivelEClica(WebElement element){
