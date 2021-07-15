76ec13287aee6720c3a6e7d372de89b42f1393a4
TimeOne01 <87388299+TimeOne01@users.noreply.github.com>
2021-07-15 09:59:49 -0300

    Atualização WebApplication.java

--- src/main/java/br/com/teamone/interfaces/WebApplication.java
+++ src/main/java/br/com/teamone/interfaces/WebApplication.java
@@ -2,6 +2,13 @@ package br.com.teamone.interfaces;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Interface para implementação do webDriver
+ * obs.: os navegadores que serão retornado no getWebDriver são configurados no Enum Web
+ */
 public interface WebApplication {
+    /**
+     * @return o webDriver que será utilizado para realizar os testes
+     */
     WebDriver getWebDriver();
 }
