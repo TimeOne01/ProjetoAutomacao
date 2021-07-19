95abe0bc9ea61fb9915a2e3ee3ddfa883d2f6710
Marcus Silva <Marcus.Silva@yaman.com.br>
2021-07-19 09:52:57 -0300

    sem texto

--- src/main/java/br/com/teamone/commons/SeleniumRobot.java
+++ src/main/java/br/com/teamone/commons/SeleniumRobot.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
+
 public class SeleniumRobot extends BaseTest {
 
     public void esperaFicarVisivelEClica(WebElement element){
